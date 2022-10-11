8ee9b1e17684a59fbd905547e554d0489101a839
Maria Coitinho <brasildu@gmail.com>
2022-10-11 14:43:09 -0500

    add NavLinks from navbar which will be used for client side routing, clicking the links and changing the url without refreshing page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Link, NavLink, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <nav>
-          Arts By Duda
+        <nav className='nav'>
+          <h1>Arts By Duda</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/About">About</NavLink>
+          <NavLink to="/Products">Products</NavLink>
         </nav>
       </BrowserRouter>
     </div>
