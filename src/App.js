1a957641162763f1e50a7dde8b0be7d080b46813
Maria Coitinho <brasildu@gmail.com>
2022-10-11 17:16:21 -0500

    update navlinks and content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import './App.css';
-import { BrowserRouter, Link, NavLink, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import './index.css'
+
+//Components
+import Home from './Components/Home';
+import About from './Components/About';
 
 function App() {
   return (
@@ -9,8 +14,13 @@ function App() {
           <h1>Arts By Duda</h1>
           <NavLink to="/">Home</NavLink>
           <NavLink to="/About">About</NavLink>
-          <NavLink to="/Products">Products</NavLink>
+          <NavLink to="/Arts">Arts</NavLink>
+          <NavLink to="/Cart">Cart</NavLink>
         </nav>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/About" element={<About/>} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
