4e05b90cd7447c95b8edf7a0d029a013fef0fe05
Maria Coitinho <brasildu@gmail.com>
2022-10-17 01:39:54 -0500

    add class name of home-page to the div that represents the Home component

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+
+function Home() {
+  return (
+    <div className="home-page">
+      <h1>Welcome</h1>
+      <p>This website is an open source art collection for artists from around the world. Here you can contribute for the art collection uploading your own art and also explore the collection.</p>
+      <img src="https://i.pinimg.com/564x/45/c0/9d/45c09ddd976b095142fc863e67c769bb.jpg" alt="site banner" />
+    </div>
+  )
+}
+
+export default Home;
