227720b2aecbbad4c237bbdbdb20bd4cecacd930
Maria Coitinho <brasildu@gmail.com>
2022-10-17 18:47:33 -0500

    Add styles to the card and art name displayed on the cards page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import './App.css';
 import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
-import './index.css'
 import { useState, useEffect } from 'react';
-
-//Components
+import './index.css'
+import './App.css';
 import Home from './components/Home';
 import About from './components/About';
 import ArtsPage from './components/ArtsPage';
@@ -24,33 +22,33 @@ function App() {
     setArts(newArtsArray)
   }
 
-return (
-  <div className="App">
-    <BrowserRouter>
-      <nav className='nav'>
-        <h1>Arts By All</h1>
-        <NavLink activeClassName="active" exact to="/">Home</NavLink>
-        <NavLink activeClassName="active" exact to="/About">About</NavLink>
-        <NavLink activeClassName="active" exact to="/Arts">Arts</NavLink>
-        <NavLink activeClassName="active" exact to="/Contribute">Contribute</NavLink>
-      </nav>
-    </BrowserRouter>
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/About">
-        <About />
-      </Route>
-      <Route path="/Arts">
-        <ArtsPage arts={arts} />
-      </Route>
-      <Route path="/Contribute">
-          <Contribute addArt={addArt}/>
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <nav className='nav'>
+          <h1>Arts By All</h1>
+          <NavLink activeClassName="active" exact to="/">Home</NavLink>
+          <NavLink activeClassName="active" exact to="/About">About</NavLink>
+          <NavLink activeClassName="active" exact to="/Arts">Arts</NavLink>
+          <NavLink activeClassName="active" exact to="/Contribute">Contribute</NavLink>
+        </nav>
+      </BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Home />
         </Route>
-    </Switch>
-  </div>
-)
+        <Route path="/About">
+          <About />
+        </Route>
+        <Route path="/Arts">
+          <ArtsPage arts={arts} />
+        </Route>
+        <Route path="/Contribute">
+            <Contribute addArt={addArt}/>
+          </Route>
+      </Switch>
+    </div>
+  )
 }
 
-export default 
+export default App;
