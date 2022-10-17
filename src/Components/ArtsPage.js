e56b48a7dc6e1e73367a1b1df15ddbbf0bcfcd2b
Maria Coitinho <brasildu@gmail.com>
2022-10-17 16:11:17 -0500

    save useRouteMatch data to constant variable of match

---
+++ src/Components/ArtsPage.js
@@ -0,0 +1,9 @@
+import { Route, useRouteMatch, Switch } from "react-router-dom";
+import ArtsList from "./ArtsList.js";
+import DisplayArt from "./DisplayArt.js";
+
+function ArtsPage({ arts }) {
+  const match = useRouteMatch()
+  console.log(match)
+
+}
