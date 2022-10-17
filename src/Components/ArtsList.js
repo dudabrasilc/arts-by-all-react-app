adfdbf075f204c79f0464ef7f83597e6c0fc2410
Maria Coitinho <brasildu@gmail.com>
2022-10-17 15:50:32 -0500

    create variable to save the useRouteMatch() hook data, ArtsList receives arts as props, artsArray returns each art card mapping through the arts props array, each art card rendered is a link to the route of each cards name {`Arts/${art.name}`}, each card is passing props to the ArtCard component

--- src/Components/ArtsList.js
+++ src/Components/ArtsList.js
@@ -1,5 +1,23 @@
-import ArtCard from "./ArtCard";
 import { Link, useRouteMatch, Route, Switch } from 'react-router-dom';
+import ArtCard from "./ArtCard";
 import DisplayArt from "./DisplayArt";
 
+function ArtsList({ arts }) {
+  const match = useRouteMatch()
+  // console.log(match)
+
+  console.log(arts)
+
+  
+  const artsArray = arts.map(art => {
+    return (
+      <li key={art.id}>
+        <Link to={`Arts/${art.name}`}>
+          <ArtCard name={art.name} artist={art.artist} image= {art.image} material={art.material} style={art.style} year={art.year}/>
+        </Link>
+      </li>
+    )
+  })
+
+}
 
