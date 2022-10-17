0c89b9ff6d317c5013ef6f96895f02c311fe4111
Maria Coitinho <brasildu@gmail.com>
2022-10-17 02:26:09 -0500

    import function components, react router dom hooks (BrowserRouter, NavLink, Route, Switch) to b eused for creating routes on the client-send server and import state hooks (useState, useEffect)

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
+import './index.css'
+import { useState, useEffect } from 'react';
+
+//Components
+import Home from './components/Home';
+import About from './components/About';
+import ArtsPage from './components/ArtsPage';
+import Contribute from './components/Contribute';
+
+function App() {
+  const [arts, setArts] = useState([])
+
+  
+}
