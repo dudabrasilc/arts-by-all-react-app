f9cbad6d2b737fa9128e37abb7f43a0480d26fa7
Maria Coitinho <brasildu@gmail.com>
2022-10-18 22:12:21 -0500

    General organization of js code file

--- src/Components/DisplayArt.js
+++ src/Components/DisplayArt.js
@@ -1,16 +1,13 @@
 import { useParams } from 'react-router-dom';
 
 function DisplayArt({ arts }) {
-
   const params = useParams()
   
   const matched = arts.filter(art => {
-    console.log(params)
     return params.artName === art.name
   })
-  console.log(matched)
+
   let matchedObj = matched[0]
-  console.log(matchedObj)
 
   if (!matchedObj) {
     return (
@@ -18,14 +15,14 @@ function DisplayArt({ arts }) {
     )
   } else {
     return (
-  <div className="card">
-    <img src={matched[0].image} alt="image" />
-    <h2>{matched[0].name}</h2>
-    <p>By: {matched[0].artist}</p>
-    <p>Drawing style: {matched[0].style}</p>
-    <p>Year: {matched[0].year}</p>
-    <p>Description: {matched[0].description}</p>
-  </div>
+      <div className="card">
+        <img src={matched[0].image} alt="image" />
+        <h2>{matched[0].name}</h2>
+        <p>By: {matched[0].artist}</p>
+        <p>Drawing style: {matched[0].style}</p>
+        <p>Year: {matched[0].year}</p>
+        <p>Description: {matched[0].description}</p>
+      </div>
     )
   }
 }
